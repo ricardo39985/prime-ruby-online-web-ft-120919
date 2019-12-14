c39f510225d61394ffac384eaa4cc9050d830fa1
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-14 16:01:47 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+      return false
+  else
+      if (2...number).any? { |i| number % i  == 0}
+          return false
+      else
+          return true
+      end
+  end
+end
