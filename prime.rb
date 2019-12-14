ca7e87a525cba6fcdcac5726c3e888b22ed7d24a
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-14 15:55:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(number)
+  bool= []
+  if num > 1
+    c=0
+    r = (2..num).to_a
+    puts r
+    puts r.length
+    while c < r.length
+        if  num % r[c]== 0 and r[c] != num
+            bool.push(false.to_s)
+        else
+          bool.push(true.to_s)
+        end
+      c+=1
+    end
+  else
+     bool.push(false.to_s)
+  end
+  puts bool
+  if bool.include? 'true'
+    return true
+  else
+    return false
+  end
+end
