ed3a382b9e5ec882a902fba6f5010cd5481371fb
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-14 15:59:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,26 +1,12 @@
 # Add  code here!
 def prime?(number)
-  bool= []
-  if num > 1
-    c=0
-    r = (2..num).to_a
-    puts r
-    puts r.length
-    while c < r.length
-        if  num % r[c]== 0 and r[c] != num
-            bool.push(false.to_s)
+  if n <= 1
+      return false
+    else 
+        if (2...n).any? { |i| n % i  == 0}
+            return false
         else
-          bool.push(true.to_s)
-        end
-      c+=1
+            return true
+        end 
     end
-  else
-     bool.push(false.to_s)
-  end
-  puts bool
-  if bool.include? 'true'
-    return true
-  else
-    return false
-  end
 end
