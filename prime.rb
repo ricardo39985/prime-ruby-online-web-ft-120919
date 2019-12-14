e5227dbe2fc5580efc782871e1c72bbe4ee2bd10
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-14 16:00:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@
 def prime?(number)
   if n <= 1
       return false
-    else
-        if (2...number).any? { |i| number % i  == 0}
-            return false
-        else
-            return true
-        end
-    end
+  else
+      if (2...number).any? { |i| number % i  == 0}
+          return false
+      else
+          return true
+      end
+  end
 end
